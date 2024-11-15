7b774c6f56b88aa92740480eb501e65f701e9fbe
babyblue <lijl@blackbeanssoft.com>
2024-11-15 15:47:51 +0800

    fix build

--- src/pages/PageThree.vue
+++ src/pages/PageThree.vue
@@ -1,7 +1,5 @@
 <template>
   <div>This is PageThree</div>
 </template>
-<script lang="ts" setup>
-  import { computed, reactive, ref, watch } from 'vue'
-</script>
+<script lang="ts" setup></script>
 <style lang="less" scoped></style>
